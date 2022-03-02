6e6c33c8d0b64ab858eccb3f90a21183137a588a
Bhavesh <bhic2030@gmail.com>
2022-03-02 11:52:13 +0530

    team update

--- data/teamsData.tsx
+++ data/teamsData.tsx
@@ -1,41 +1,58 @@
 const teamsData = [
   {
     id: 1,
+    image: "/images/team/aryaDeshmukh.png",
+    title: "General Secretary",
+    name: "Arya Deshmukh"
+  },
+  {
+    id: 2,
+    image: "/images/team/tanishkSheware.png",
+    title: "Cultural Secretary",
+    name: "Tanishk Sheware"
+  },
+  {
+    id: 3,
     image: "/images/team/vedantRathi.png",
     title: "Event Management",
     name: "Vedant Rathi"
   },
   {
-    id: 2,
+    id: 4,
     image: "/images/team/sanskarDeoghare.png",
     title: "Corporate",
     name: "Sanskar V Deoghare"
   },
   {
-    id: 3,
+    id: 5,
     image: "/images/team/srinivasNaik.png",
     title: "Social Media",
     name: "Srinivas Naik"
   },
-   {
-    id: 4,
+  {
+    id: 6,
+    image: "/images/team/sakshiPandagale.png",
+    title: "Content",
+    name: "Sakshi Pandagale"
+  },
+  {
+    id: 7,
     image: "/images/team/mohitKothari.png",
     title: "Design",
     name: "Mohit Kothari"
   },
   {
-    id: 5,
+    id: 8,
     image: "/images/team/jatinChhangani.png",
     title: "Web Development",
     name: "Jatin Chhangani"
   },
   {
-    id: 6,
+    id: 9,
     image: "/images/team/bhaveshChaudhari.png",
     title: "Web Development",
     name: "Bhavesh Chaudhari"
-  },
- 
+  }
 ];
 
 export default teamsData;
